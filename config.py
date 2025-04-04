2e70fcc45d3d4431ce74c9545be0773aaf05b1f0
plhannakabakj <kagdhzisbabahakajjzk@outlook.com>
2025-04-04 23:18:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ BOT_TOKEN = getenv("7849762569:AAFng-G1P5bI2vVSI_X7POIRUSPYn7Q", None)
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", None)
+MUSIC_BOT_NAME = getenv("MusicXsweet", None)
 PRIVATE_BOT_MODE = getenv("PRIVATE_BOT_MODE", None)
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 900))
